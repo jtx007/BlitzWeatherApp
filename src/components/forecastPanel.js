b56b978929402818bf456699d388c2aca67f45a4
jtx007 <jamesjacobthomas7@gmail.com>
2018-12-19 23:57:21 -0500

    set up initial fetch, need to setup proxy server for cors

--- src/components/forecastPanel.js
+++ src/components/forecastPanel.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react'
 import secret from '../secret'
 import getWeatherData from '../adapters'
+
+
 export default class forecastPanel extends Component {
+
+
+
     render () {
-        console.log(secret, getWeatherData)
+         getWeatherData(secret, 37.8267, -122.4233)
+         .then(r => r.json())
+         .then(data => console.log(data))
+        
         return (
             <div className="forecast-panel">
 
