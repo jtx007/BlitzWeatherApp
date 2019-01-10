cd911a39c4f8b13224a157205e48bc0461d9ac09
jtx007 <jamesjacobthomas7@gmail.com>
2019-01-09 20:58:41 -0500

    initialized weather panels

--- src/components/forecastPanel.js
+++ src/components/forecastPanel.js
@@ -5,16 +5,34 @@ import getWeatherData from '../adapters'
 
 export default class forecastPanel extends Component {
 
-
+    componentDidMount() {
+        getWeatherData(secret, 37.8267, -122.4233)
+        .then(r => r.json())
+        .then(data => console.log(data))
+        
+    }
 
     render () {
-         getWeatherData(secret, 37.8267, -122.4233)
-         .then(r => r.json())
-         .then(data => console.log(data))
         
         return (
             <div className="forecast-panel">
-
+                <div className="weather-panels">
+                    <div className="day-panel">
+                        <p>Day 1</p>
+                    </div>
+                    <div className="day-panel">
+                        <p>Day 2</p>
+                    </div>
+                    <div className="day-panel">
+                        <p>Day 3</p>
+                    </div>
+                    <div className="day-panel">
+                        <p>Day 4</p>
+                    </div>
+                    <div className="day-panel">
+                        <p>Day 5</p>
+                    </div>
+                </div>
             </div>
         )
     }
