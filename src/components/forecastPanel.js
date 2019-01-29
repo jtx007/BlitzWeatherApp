95a15e44b7353f4d45e971845c3c4175039bf46b
jtx007 <jamesjacobthomas7@gmail.com>
2019-01-28 23:01:19 -0500

    added splash page placeholder for empty space before weather search

--- src/components/forecastPanel.js
+++ src/components/forecastPanel.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import Skycons from 'react-skycons'
 import DayPanel from './DayPanel.js'
 
 
@@ -13,9 +14,59 @@ export default class forecastPanel extends Component {
 
     
     renderWeatherPanels = (data) => {
+        if (data === undefined || data.length === 0) {
+            return (
+            <div className="splash-page-panel">
+                <Fragment>
+                        <div className="splash-page-icons">
+                            <h1>Welcome to Blitz</h1>
+                            <div className="icon sun-shower">
+                                <div className="cloud">
+                                </div> 
+                                <div className="sun" >
+                                <div className="rays" > </div> 
+                                </div> 
+                                <div className="rain"> 
+                                </div> 
+                            </div>
+                            <div className="icon thunder-storm">
+                                <div className="cloud"></div> 
+                                    <div className="lightning">
+                                        <div className="bolt"></div> 
+                                        <div className="bolt"></div> 
+                                    </div> 
+                            </div>
+                            <div className="icon cloudy">
+                                <div className="cloud"></div> 
+                                <div className="cloud"></div> 
+                            </div>
+                            <div className="icon flurries">
+                                <div className="cloud"></div> 
+                                <div className="snow">
+                                    <div className="flake"></div> 
+                                    <div className="flake"></div> 
+                                </div> 
+                            </div>
+                            <div className="icon sunny">
+                                <div className="sun">
+                                    <div className="rays"></div> 
+                                </div> 
+                            </div>
+                            <div className="icon rainy">
+                                <div className="cloud"></div> 
+                                <div className="rain"></div> 
+                            </div>
+
+                        </div>
+                </Fragment>
+            </div>
+
+            )
+        } else {
         return data.map(day => {
             return <div key={day.time} className="day-panel"><DayPanel key={day.time} weather={day}/></div>
-        })
+        }
+        )}
     }
 
     handleSearchBox = (e) => {
@@ -46,6 +97,7 @@ export default class forecastPanel extends Component {
                     <button className="search-btn" type="submit">Search</button> 
                 </form>
                 <div className="forecast-panel">
+
                     {this.renderWeatherPanels(this.state.weather)}
                 </div>
             </div>
